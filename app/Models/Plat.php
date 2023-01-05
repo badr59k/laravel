b7eff9c26e84dfe8f44748f0d1470d08a1b9c801
badr59k <khelifi.badr.1@gmail.com>
2023-01-05 14:52:09 +0100

    Ajout des relations des modèles et des seeders
    Création d'un script de reconstruction de la BDD
    
    Ajout des relations dans les modèles PhotoPlat et Plat
    
    Ajout des seeders pour chaque modèle

---
+++ app/Models/Plat.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plat extends Model
+{
+    use HasFactory;
+
+    protected $table = 'plat';
+    protected $primaryKey = 'id';
+
+
+    /**
+     * Cette fonction permet de récuperer la photo
+     * 
+     * @return PhotoPlat
+     */
+    public function photo()
+    {
+        return $this ->hasOne(PhotoPlat::class);
+    }
+}
