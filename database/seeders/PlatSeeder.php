bd4ab3e5ecd96e149970f0bcc20d40a4e13892fe
badr59k <khelifi.badr.1@gmail.com>
2023-01-06 13:51:46 +0100

    Modification et amélioration de seeders
    Ajout de données aléatoires dans les seeder Plat et Reservation
    -> En utilisant Faker
    
    Ajout du seeder Reservation dans le seeder Database

--- database/seeders/PlatSeeder.php
+++ database/seeders/PlatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker;
 use App\Models\Categorie;
 use App\Models\PhotoPlat;
 use App\Models\Plat;
@@ -17,8 +18,12 @@ class PlatSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         // toutes les catégories
         $categories = Categorie::all();
+        // le nombre d'éléments dans la collection
+        $categoriesCount = $categories->count();
         // la première catégorie
         $categorieEntree = $categories->first();
         // la deuxième catégorie (id 2, plat)
@@ -72,7 +77,35 @@ class PlatSeeder extends Seeder
             $plat->photo_plat_id = $platData['photo_plat_id'];
             $plat->categorie_id = $platData['categorie_id'];
             $plat->save();
-    
+        }
+
+        for ($i = 0; $i < 100; $i++){
+
+            $plat = new Plat();
+
+            $plat->nom = $faker->words(2, true);
+
+            $plat->description = $faker->words(10, true);
+
+            $plat->prix = random_int(100,1000)/100;
+            // équivalent de la ligne du dessus mais avec Faker
+            // $plat->prix = $faker->randomFloat(2, 1, 50);
+
+            //le statut épinglé est aléatoire, 0 == false, 1 == true
+            $plat->epingle = (bool) random_int(0, 1);
+
+            // affectation d'une photo
+            $plat->photo_plat_id = $photo->id;
+
+            // affectation d'une catégorie
+            // la catégorie est choisie au hasard
+            // un nombre aléatoire est tiré entre 0 et 5-1 (donc 4)
+            $categorieIndex = random_int(0, $categoriesCount - 1);
+            // on utilise le nombre tiré au hasard pour accéder au Nième éléments de la collection de catégories
+            $categorie = $categories->get($categorieIndex);
+            $plat->categorie_id = $categorie->id;
+
+            $plat->save();
         }
     }
 }
--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker;
+use App\Models\Reservation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,70 @@ class ReservationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create('fr_FR');
+
+        // 2 réservations avec des données statiques
+        
+        $reservationDatas = [
+            [
+                'nom' => 'Foo',
+                'prénom' => 'Foo',
+                'jour' => '2023-01-06',
+                'heure' => '12:00' ,
+                'nombre_personnes' => 4,
+                'tel' => '0612345678',
+                'email' => 'foo.foo@exemple.com'
+            ],
+            [
+                'nom' => 'Bar',
+                'prénom' => 'Bar',
+                'jour' => '2023-01-13',
+                'heure' => '12:00' ,
+                'nombre_personnes' => 8,
+                'tel' => '0634567812',
+                'email' => 'bar.bar@exemple.com'
+            ],
+        ];
+
+        foreach ($reservationDatas as $reservationData) {
+            $reservation = new Reservation();
+            $reservation->nom = $reservationData['nom'];
+            $reservation->prenom = $reservationData['prénom'];
+            $reservation->jour = $reservationData['jour'];
+            $reservation->heure = $reservationData['heure'];
+            $reservation->nombre_personnes = $reservationData['nombre_personnes'];
+            $reservation->tel = $reservationData['tel'];
+            $reservation->email = $reservationData['email'];
+            $reservation->save();
+        }
+        
+        // 48 réservations avec des données aléatoires
+        for ($i = 0; $i < 48; $i++) {
+            
+            $reservation = new Reservation();
+            // nom
+            $reservation->nom = $faker->lastName();
+
+            // prénom
+            $reservation->prenom = $faker->FirstName();
+
+            // jour
+            $reservation->jour = $faker->date('Y-m-d');
+
+            // heure
+            $reservation->heure = $faker->time('H:i');
+
+            // le nombre de personne
+            // utiliser faker random
+            $reservation->nombre_personnes = random_int(1,8);
+
+            //tel
+            $reservation->tel = $faker->phoneNumber();
+
+            //email
+            $reservation->email = $faker->safeEmail();
+
+            $reservation->save();
+        }
     }
 }
