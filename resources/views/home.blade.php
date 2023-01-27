cc652587946d2edd5aa06a69787ea0f8a9838d04
badr59k <khelifi.badr.1@gmail.com>
2023-01-27 10:00:45 +0100

    Code style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,8 @@
 @section ('page_title', 'Accueil')
 
 @section('content') 
-    <img class="medium-size" 
-         src="{{asset('img/Image restaurant.jpg')}}"  
-         alt="Interieur du restaurant">
-    <img class="medium-size" 
-         src="{{asset('img/cuisinier.jpg')}}"  
-         alt="cuisinier">
+    <img class="medium-size" src="{{asset('img/Image restaurant.jpg')}}" alt="Interieur du restaurant">
+    <img class="medium-size" src="{{asset('img/cuisinier.jpg')}}"  alt="cuisinier">
 
 <h3> Dernières actualités </h3>
 
