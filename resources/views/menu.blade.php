5c32f780f9778067ca0f2a4b8cab006aa707834d
badr59k <khelifi.badr.1@gmail.com>
2023-03-02 11:09:20 +0100

    Amélioration de la page menu
    Création d'un tableau pour l'affichage des plats

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,22 +3,28 @@
 @section ('page_title', 'Menu')
 
 @section('content')
-    <h2> Menu </h2>
-    @foreach ($categories as $categorie)
-        <h4>{{$categorie->nom}}</h4>
-        <p> {{$categorie->description }}</p>
-
-        <ul>
-            @foreach ($categorie->platsSortedByNom as $plat)
-            <li>
-                <img class="menu--photo-plat" src="{{ asset($plat->photo->chemin) }}" alt="">
-                {{ $plat->nom }} {{ $plat->prix }} eur<br>
-                {{ $plat->description}}<br>
-                @foreach ($plat->etiquettes as $etiquette)
+    <center>    
+        <h2> Menu </h2>
+        @foreach ($categories as $categorie)
+            <table class="menu" width="50%">
+                <tr>
+                    <td>
+                        <h4>{{$categorie->nom}}</h4>
+                        {{$categorie->description }}
+                    </td>
+                </tr>
+                <td>
+                    @foreach ($categorie->platsSortedByNom as $plat)
+                    <img class="menu--photo-plat" src="{{ asset($plat->photo->chemin) }}" alt="">
+                    <h4>{{ $plat->nom }} {{ $plat->prix }}€</h4>
+                    {{ $plat->description}}<br>
+                    @foreach ($plat->etiquettes as $etiquette)
                     #{{ $etiquette->nom }}
-                @endforeach
-            </li>
-            @endforeach
-        </ul>
-    @endforeach
-@endsection
+                    @endforeach
+                    <br><br><br>
+                    @endforeach
+                </td>
+            </table>
+        @endforeach
+    </center>
+        @endsection
