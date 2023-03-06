e55a0e89719ca671dd7958fef5810e90f3adede9
badr59k <khelifi.badr.1@gmail.com>
2023-03-06 15:05:51 +0100

    Création d'un formulaire de réservation pour les utilisateurs
    Attention, le formulaire de réservation n'entre pas les données dans la BDD

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -4,4 +4,39 @@
 
 @section('content')
 <h2> Réservation </h2>
+    <form>
+        <div class="corps_formulaire">
+            <div>
+                <label>Nom</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Prenom</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Jour</label>
+                <input type="date">
+            </div>
+            <div>
+                <label>Heure</label>
+                <input type="time">
+            </div>
+            <div>
+                <label>Couverts</label>
+                <input type="number">
+            </div>
+            <div>
+                <label>Tel</label>
+                <input type="tel" placeholder="06 12 34 56 78">
+            </div>
+            <div>
+                <label>Adresse mail</label>
+                <input type="email" placeholder="ocnamo@contact.net">
+            </div>
+        </div>
+        <div >
+            <button>Réserver</button>
+        </div>
+    </form>
 @endsection
