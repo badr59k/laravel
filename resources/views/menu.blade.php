fda494e516ce2e8a0be12237dd12bc49f5fc9aaf
badr59k <khelifi.badr.1@gmail.com>
2023-03-09 23:44:20 +0100

    Changement du menu
    Création de lien pour aller directement à une catégorie
    
    Amélioration CSS du menu du restaurant

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,22 +5,45 @@
 @section('content')
     <center>    
         <h2> Menu </h2>
+        <table width="50%">
+            <tr>
+                <th>
+                    <a href="#ancre1">Entrée</a>
+                </th>
+                <th>
+                    <a href="#ancre2">Plat</a>
+                </th>
+                <th>
+                    <a href="#ancre3">Dessert</a>
+                </th>
+                <th>
+                    <a href="#ancre4">Petit-Déjeuner</a>
+                </th>
+                <th>
+                    <a href="#ancre5">Boisson</a>
+                </th>
+            </tr>
+        </table>
         @foreach ($categories as $categorie)
             <table class="menu" width="50%">
                 <tr>
                     <td>
-                        <h4>{{$categorie->nom}}</h4>
+                        <div id="ancre{{$categorie->id}}">
+                            <h4>{{$categorie->nom}}</h4>
+                        </div>
                         {{$categorie->description }}
                     </td>
                 </tr>
                 <td>
                     @foreach ($categorie->platsSortedByNom as $plat)
-                    <img class="menu--photo-plat" src="{{ asset($plat->photo->chemin) }}" alt="">
-                    <h4>{{ $plat->nom }} {{ $plat->prix }}€</h4>
-                    {{ $plat->description}}<br>
-                    @foreach ($plat->etiquettes as $etiquette)
-                    #{{ $etiquette->nom }}
-                    @endforeach
+                    <center>
+                        <img class="menu--photo-plat" src="{{ asset($plat->photo->chemin) }}" alt="">
+                        <h4>{{ $plat->nom }} {{ $plat->prix }}€</h4>
+                        {{ $plat->description}}<br>
+                        @foreach ($plat->etiquettes as $etiquette)
+                        #{{ $etiquette->nom }}
+                        @endforeach
+                    </center>
                     <br><br><br>
                     @endforeach
                 </td>
