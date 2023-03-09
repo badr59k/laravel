69813a24f89fef875544c0269b3a2596f497c455
badr59k <khelifi.badr.1@gmail.com>
2023-03-09 11:51:39 +0100

    Changement des photos d'ambiance de la homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
 @section('content') 
     <center>    
         <div id="caroussel">
-            <div class="image-ambiance">
-                <img src="{{asset('img/Image restaurant.jpg')}}" alt="Interieur du restaurant">
-                <img src="{{asset('img/cuisinier.jpg')}}"  alt="cuisinier">
-                <img src="{{asset('img/clem-onojeghuo-zlABb6Gke24-unsplash.jpg')}}"  alt="resto exterieur">
-                <img src="{{asset('img/jay-wennington-N_Y88TWmGwA-unsplash.jpg')}}"  alt="ambiance">
+            <div class="images-accueil">
+                <img class="image-ambiance" src="{{asset('img/ambiance/6f760bd2-7463-48d6-be2a-ee7a48bfcc56.jfif')}}" alt="Interieur du restaurant">
+                <img class="image-ambiance" src="{{asset('img/ambiance/43c17afd-284f-48fc-b35c-8c0e2cf8e464.jfif')}}" alt="Interieur du restaurant">
+                <img class="image-ambiance" src="{{asset('img/ambiance/bfd37413-91ad-4e1a-b697-f40f3742d99a.jfif')}}" alt="Interieur du restaurant">
+                <img class="image-ambiance" src="{{asset('img/ambiance/eaf30327-e0ae-4066-8ecc-a5b816344447.jfif')}}" alt="Interieur du restaurant">
             </div>
             <a href="{{ route('reservation')}}"><button id="bouton-reservation"> Réserver</button></a>
         </div>
