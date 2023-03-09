58a2dc1c073864035a8fdfdbce08760e33b390f2
badr59k <khelifi.badr.1@gmail.com>
2023-03-09 23:40:37 +0100

    Ajout de texte aléatoire dans la page mention legales

--- resources/views/mentions_legales.blade.php
+++ resources/views/mentions_legales.blade.php
@@ -4,4 +4,9 @@
 
 @section('content')
 <h2> Mentions Légales </h2>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, ratione, possimus consequuntur earum fuga totam vitae reiciendis quod, illum nesciunt enim aliquid cumque quaerat quo neque dolores ut repellendus itaque?</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, ratione, possimus consequuntur earum fuga totam vitae reiciendis quod, illum nesciunt enim aliquid cumque quaerat quo neque dolores ut repellendus itaque?</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, ratione, possimus consequuntur earum fuga totam vitae reiciendis quod, illum nesciunt enim aliquid cumque quaerat quo neque dolores ut repellendus itaque?</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, ratione, possimus consequuntur earum fuga totam vitae reiciendis quod, illum nesciunt enim aliquid cumque quaerat quo neque dolores ut repellendus itaque?</p>
+
 @endsection
